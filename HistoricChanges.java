070576acd178f027ade3ff93ae59dc7bf02ba9bc
Tiago Coelho Costa <tiago_sa@msn.com>
2019-04-14 12:43:28 -0300

    update class

--- HistoricChanges.java
+++ HistoricChanges.java
@@ -1,21 +1,36 @@
-class HistoricChanges{
+public class HistoricChanges{
 	private List<AbstractMap.SimpleEntry<String, Runnable[]>> changes = new ArrayList<AbstractMap.SimpleEntry<String,Runnable[]>>();
 	private int index = 0;
 	private int maxChanges = 10;
 	public HistoricChanges(int maxChanges){
-		this.maxChanges=maxChanges;
-	}
-	public String getLastChangeName(){return changes.get(changes.size()-1).getKey();}
-	public String getIndexChangeName(){return changes.get(index).getKey();}
-	public String[] getAllChangeName(){
-		String[] names= new String[changes.size()];
-		for(int i = 0; i < changes.size(); i++){
-			AbstractMap.SimpleEntry<String, Runnable[]> entry = changes.get(i);
-			names[i]= entry.getKey();
+		synchronized(changes){
+			this.maxChanges=maxChanges;
 		}
-		return names;
 	}
-	
+	public String getLastChangeName(){
+		synchronized(changes){
+			return changes.get(changes.size()-1).getKey();
+		}
+	}
+	public String getIndexChangeName(){
+		synchronized(changes){
+			return changes.get(index).getKey();
+		}
+	}
+	public String[] getAllChangeName(){
+		synchronized(changes){
+			String[] names= new String[changes.size()];
+			for(int i = 0; i < changes.size(); i++){
+				AbstractMap.SimpleEntry<String, Runnable[]> entry = changes.get(i);
+				names[i]= entry.getKey();
+			}
+			return names;
+		}
+	}
+	public void clear(){
+		changes.clear();
+		index = 0;
+	}
 	public void addChange(String name, Runnable undoAction, Runnable redoAction){
 		Runnable[] change = new Runnable[2];
 		change[0] = undoAction;
@@ -35,20 +50,26 @@ class HistoricChanges{
 		return index > 0;
 	}
 	public boolean isRedable(){
-		return index < changes.size();
+		int size = 0;
+		synchronized(changes){size = changes.size();}
+		return index < size;
 	}
 	public boolean undo(){
 		if(isUndable()){
-			index--;
-			changes.get(index).getValue()[0].run();
+			synchronized(changes){
+				index--;
+				changes.get(index).getValue()[0].run();
+			}
 		}
-		
+
 		return isUndable();
 	}
 	public boolean redo(){
 		if(isRedable()){
-			changes.get(index).getValue()[1].run();
-			index++;
+			synchronized(changes){
+				changes.get(index).getValue()[1].run();
+				index++;
+			}
 		}
 
 		return isRedable();
